ef08e659539e8500250d31fd8c299b0b4211b02f
Johan Nyberg <johan.e.nyberg@icloud.com>
2021-02-04 15:27:06 +0100

    DynamoDB

--- src/main/java/net/roburterra/guestbook/SimpleGuestbookApplication.java
+++ src/main/java/net/roburterra/guestbook/SimpleGuestbookApplication.java
@@ -1,13 +1,31 @@
 package net.roburterra.guestbook;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import net.roburterra.guestbook.dao.GuestbookMessageRepository;
+import net.roburterra.guestbook.domain.GuestbookMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SimpleGuestbookApplication {
 
+	@Autowired
+	GuestbookMessageRepository guestbookMessageRepository;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SimpleGuestbookApplication.class, args);
+	}
+
+	@Component
+	public class Runner implements CommandLineRunner{
+		@Override
+		public void run(String... args) throws Exception {
+			guestbookMessageRepository.findAll().stream().forEach(System.out::println);
+		}
 	}
 
 }
--- src/main/java/net/roburterra/guestbook/dao/GuestbookMessageRepository.java
+++ src/main/java/net/roburterra/guestbook/dao/GuestbookMessageRepository.java
@@ -1,10 +1,63 @@
 package net.roburterra.guestbook.dao;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import net.roburterra.guestbook.domain.GuestbookMessage;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface GuestbookMessageRepository extends CrudRepository<GuestbookMessage,Long> {
+@EnableScan
+public interface GuestbookMessageRepository extends CrudRepository<GuestbookMessage,String> {
     List<GuestbookMessage> findAll();
+
+    @Configuration
+    @EnableDynamoDBRepositories(basePackageClasses = GuestbookMessageRepository.class)
+    public static class DynamoDBConfig {
+
+        @Value("${amazon.aws.accesskey}")
+        private String amazonAWSAccessKey;
+
+        @Value("${amazon.aws.secretkey}")
+        private String amazonAWSSecretKey;
+
+        public AWSCredentialsProvider amazonAWSCredentialsProvider() {
+            return new AWSStaticCredentialsProvider(amazonAWSCredentials());
+        }
+
+        @Bean
+        public AWSCredentials amazonAWSCredentials() {
+            return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+        }
+
+        @Bean @Primary
+        public DynamoDBMapperConfig dynamoDBMapperConfig() {
+            return DynamoDBMapperConfig.DEFAULT;
+        }
+
+        @Bean @Primary
+        public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB, DynamoDBMapperConfig config) {
+            return new DynamoDBMapper(amazonDynamoDB, config);
+        }
+
+        @Bean
+        public AmazonDynamoDB amazonDynamoDB() {
+            return AmazonDynamoDBClientBuilder.standard().withCredentials(amazonAWSCredentialsProvider())
+                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://dynamodb.eu-north-1.amazonaws.com","eu-north-1"))
+                    .build();
+        }
+    }
 }
